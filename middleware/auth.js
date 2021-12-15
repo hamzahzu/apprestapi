585b0c7e00e26cb9a22b65822035e637e8d02710
Hamzah Zakaria <hamzahzakaria92@gmail.com>
2021-12-15 11:00:28 +0700

    login user with jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,7 @@ const response = require('../res');
 const jwt = require('jsonwebtoken');
 const config = require('../config/secret');
 const ip = require('ip');
+const { connect } = require('../koneksi');
 
 //controller register
 exports.registrasi = function(req, res) {
@@ -59,6 +60,81 @@ exports.registrasi = function(req, res) {
                     message = "E-mail registered!";
                     response.ok(code, message, rows, total, stat, res);
                     //response.ok("E-mail registered!", res);
+                }
+            }
+        })
+}
+
+exports.login = function(req, res) {
+    let post = {
+        password: req.body.password,
+        email: req.body.email
+    }
+
+    let code, message, total, stat = '';
+
+    let query = "SELECT * FROM  ?? WHERE ?? = ? AND ?? =? ";
+    let table = ["node_mysql.user", "password", md5(post.password), "email", post.email];
+
+    query = mysql.format(query, table);
+
+    connection.query(query,
+        function(error, rows) {
+            if (error) {
+                console.log(error);
+                code = 500;
+                stat = "error";
+                message = error.sqlMessage;
+                response.ok(code, message, rows, total, stat, res);
+            } else {
+                if (rows.length == 1) {
+                    let token = jwt.sign({ rows }, config.secret, {
+                        expiresIn: 1440 //25 minutes
+                    });
+                    id_user = rows[0].id;
+
+                    let data = {
+                        id_user: id_user,
+                        access_token: token,
+                        ip_address: ip.address()
+                    }
+
+                    let query = "INSERT INTO ?? SET ?";
+                    let table = ["node_mysql.akses_token"];
+
+                    query = mysql.format(query, table);
+                    connection.query(query, data, function(error, rows) {
+                        if (error) {
+                            console.log(error);
+                            code = 500;
+                            stat = "error";
+                            message = error.sqlMessage;
+                            response.ok(code, message, rows, total, stat, res);
+                        } else {
+                            res.json({
+                                success: true,
+                                message: "JWT tokens are generated!",
+                                token: token,
+                                currUser: data.id_user
+                            });
+                            /*  code = 200;
+                             stat = "sucess";
+                             message = "JWT tokens are generated";
+                             total = rows.length;
+                             response.ok(code, message, rows, total, stat, res); */
+                        }
+                    });
+
+
+                } else {
+                    res.json({
+                        error: true,
+                        message: "E-mail or password incorrect!",
+                    });
+                    /* code = 501;
+                    stat = "error";
+                    message = "E-mail registered!";
+                    response.ok(code, message, rows, total, stat, res); */
                 }
             }
         })
